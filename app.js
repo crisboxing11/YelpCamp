baafb1f7f9d266f1af506b7e557f3fe08fcef466
crisboxing11 <crisboxing11@gmail.com>
2022-01-20 19:27:52 -0800

    Updated Schema

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require('path');
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
