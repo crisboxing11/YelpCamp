e5b8403be5c22554c87f568ef2473b238f3dd6e8
crisboxing11 <crisboxing11@gmail.com>
2022-01-23 23:20:54 -0800

    adding seed helpers

--- seedHelpers.js
+++ seedHelpers.js
@@ -18,3 +18,27 @@ const descriptors = [
   "Dusty",
   "Diamond",
 ];
+
+const places = [
+  "Flats",
+  "Village",
+  "Canyon",
+  "Pond",
+  "Group Camp",
+  "Horse Camp",
+  "Ghost Town",
+  "Camp",
+  "Dispersed Camp",
+  "Backcountry",
+  "River",
+  "Creek",
+  "Creekside",
+  "Bay",
+  "Spring",
+  "Bayshore",
+  "Sands",
+  "Mule Camp",
+  "Hunting Camp",
+  "Cliffs",
+  "Hollow",
+];
