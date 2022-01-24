bd117d44b120528e69989f5143fe7f06171658d6
crisboxing11 <crisboxing11@gmail.com>
2022-01-23 23:27:41 -0800

    created database in index

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose");
-const Campground = require("./models/campgrounds");
+const Campground = require("../models/campgrounds");
 
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -14,3 +14,12 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
+
+
+const seedDB= async () => {
+    await Campground.deleteMany({});
+    const c = new Campground({title: 'purple field'});
+    await c.save();
+}
+
+seedDB();
