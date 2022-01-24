7d81e51376311c75561d9a00fb6fe9375e06891c
crisboxing11 <crisboxing11@gmail.com>
2022-01-23 23:23:12 -0800

    adjusted project structure

---
+++ seeds/index.js
@@ -0,0 +1,16 @@
+
+const mongoose = require("mongoose");
+const Campground = require("./models/campgrounds");
+
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
